9fdb9ae31bb1e85c57ea23b8be9798e0740ecb50
Gorock Shetty <gorock397@gmail.com>
2023-02-27 21:43:41 +1100

    webpack configured with required loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,64 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
     mode: 'development',
     module: {
         rules: [
             {
-                test: /\.svg$/,
-                use: [
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options:{
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.(mp4|webm)$/,
+                use:[
                     {
-                        loader: "svg-inline-loader"
+                        loader:'url-loader',
+                        options:{
+                            limit: 8192,
+                            name: 'videos/bg2.mp4'
+                        }
                     }
                 ]
-            }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ]
     },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        port: 3000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
     resolve: {
         extensions: [".js", ".jsx", ".svg"]
     },
-    output: {
-        path: path.join(__dirname, "dist"),
-        publicPath: "/",
-        filename: "bundle.js"
-    }
 };
